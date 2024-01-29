bbd3f2b9b3da88934c7717edaca601bea350aa4c
christiano112 <enyiachristian12@gmail.com>
2024-01-29 19:01:12 +0100

    continued integration

--- src/octosoft-styleguide.tsx
+++ src/octosoft-styleguide.tsx
@@ -1,7 +1,7 @@
 // Anything exported from this file is importable by other in-browser modules.
 
 // Importing the CSS file this way so the CSS is not treated as a module but as a global CSS file.
-import "./styles/global.css?modules=false";
+import "./global.css?modules=false";
 
 // import components
 import AppButton from "./components/button";
